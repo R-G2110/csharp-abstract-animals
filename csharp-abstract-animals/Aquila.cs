f4cc298205e6e03a283bec12dc71bcf4d595f610
R-G2110 <argeldelacruz86@gmail.com>
2024-04-24 14:42:21 +0200

    [Add]: class Aquila

---
+++ csharp-abstract-animals/Aquila.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_abstract_animals
+{
+    public class Aquila : Animale
+    {
+        // Sovrascrizione del metodo Verso nella classe l'Aquila
+        public override void Verso()
+        {
+            Console.WriteLine("Screech!");
+        }
+        // Sovrascrizione del metodo Mangia nella classe l'Aquila
+        public override void Mangia()
+        {
+            Console.WriteLine("Mangia carne.");
+        }
+    }
+}
