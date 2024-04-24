c8a92c67463cd663aa3dc5c2dd213b2d204de305
R-G2110 <argeldelacruz86@gmail.com>
2024-04-24 14:19:41 +0200

    [Add]: abstract class Animale

---
+++ csharp-abstract-animals/Animale.cs
@@ -0,0 +1,15 @@
+﻿namespace csharp_abstract_animals
+{
+    public abstract class Animale
+    {
+        // Metodo Dormi da implementare nella classe astratta
+        public void Dormi()
+        {
+            Console.WriteLine("Zzz");
+        }
+        // Metodo Verso da implementare nelle classi derivate
+        public abstract void Verso();
+        // Metodo Mangia da implementare nelle classi derivate
+        public abstract void Mangia();
+    }
+}
