fe7699a20091bd7e59af05186a7551451fc07c0a
R-G2110 <argeldelacruz86@gmail.com>
2024-04-24 14:26:10 +0200

    [Add]: class Cane

---
+++ csharp-abstract-animals/Cane.cs
@@ -0,0 +1,16 @@
+﻿namespace csharp_abstract_animals
+{
+    public class Cane : Animale
+    {
+        // Sovrascrizione del metodo Verso nella classe Cane
+        public override void Verso()
+        {
+            Console.WriteLine("Bau! Bau!");
+        }
+        // Sovrascrizione del metodo Mangia nella classe Cane
+        public override void Mangia()
+        {
+            Console.WriteLine("Mangia carne.");
+        }
+    }
+}
